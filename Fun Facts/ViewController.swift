3adc7959315d98f45c579f00cfcdac455a16026b
Brandon <leebran@onid.oregonstate.edu>
2015-06-27 20:58:22 -0700

    Implemented MVC

--- Fun Facts/ViewController.swift
+++ Fun Facts/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        funFactLabel.text = factBook.factsArray[0]
+        funFactLabel.text = factBook.getRandomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
-        funFactLabel.text = factBook.factsArray[1]
+        funFactLabel.text = factBook.getRandomFact()
     }
 
 }
